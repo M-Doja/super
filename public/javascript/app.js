1f16eef16b05d298bc271e529272b44153e1f89c
M-Doja <micdoja@yahoo.com>
2016-03-16 00:52:09 -0700

    design tweeks

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -7,18 +7,18 @@ $(document).ready(function(){
         console.log(to);
         if (text == "" && subject == "") {
           console.log('No data entered. Send cancelled');
-          $("#message").html("<p>There is missing information!</p>");
+          $("#message").html("<p style='font-family:arial black'>There is missing information!</p>");
           return;
         }
-        $("#message").html("<p>Sending Message...Please wait</p>");
+        $("#message").html("<p style='font-family:arial black'>Sending Message...Please wait</p>");
         $.get("/send",{to:to,subject:subject,text:text},function(data){
-        if(data=="sent")
-        {
-            $("#message").empty().html("<br><br><br><h4 style='color:white;font-family:serif;margin-top:-60px;'>Email has been sent to "+to+" .<br> Thanks for the message! I'll get back to you as soon as I can.</h4>");
-        }
+        // if(data=="sent")
+        // {
+        //     $("#message").empty().html("<br><br><br><h4 id='sent'>Email has been sent to "+to+".<br> Thanks for the message! I'll get back to you as soon as I can.</h4>");
+        // }
       });
       document.getElementById("subject").value = '';
       document.getElementById("text").value = '';
     });
-    
+
 });
